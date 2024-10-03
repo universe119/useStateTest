122e16a56f0bedece8359d4c0219b14a2d4aff2c
universe119 <ruddh119@gmail.com>
2024-10-04 00:30:03 +0900

    useState 완전복사를 통한 컴포넌트 재랜더링(불변성)

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,42 @@
 //useState : state를 생성하거나 해당 state를 변경해서 컴포넌트를 재렌더링 시켜줄수 있는 state변경함수 반환
 import { useState } from "react";
 
+// 불변성 immutable : 값이 훼손되지 않는, 불변한
+// 리액트는 기본적으로 원본, 변경될 자료가 모두 있어야됨
+// 참조형 자료를 그냥 다른 변수에 옮겨 담는것은 힙메모리에 있는 값 자체를 복사하는 것이 아닌 해당 값의 참조값만 복사가 됨 (얕은복사 Shallow Copy)
+// 얕은 복사가 된 참조형 자료를 변경하면 원본의 훼손이 일어나면서 불변성이 유지가 안됨
+// 리액트입장에서 불변성이 깨지면 비교할 대상이 없어진다는 의미이므로 화면 재랜더링 자체가 안됨
+// -- 전개연산자를 통한 원본 참조형 자료를 완전 복사 후 완전복사된 값을 변경해서 state변경함수 처리
+
 export default function App() {
-  console.log("App render");
-  let [Num, setNum] = useState(0) // 비구조할당으로 받음, 보통 일반적으로 프론트개발자들이 이런식으로 씀, 강사님 개인적으로 useState를 쓸때 변수 앞글자를 대문자를 씀
+	console.log("App render");
+	const [Colors, setColors] = useState(["red", "green", "blue"]); // 배열값이 담긴
+	// let [Num, setNum] = useState(0); // 원시형자료
 
-  const plusNum = () => {
-    setNum(Num + 1);
-    console.log(Num);
-  };
+	const changeColors = () => {
+		const newColors = [...Colors]; // 기존의 참조값만 복사하는 것이 값 자체를 완전복사(원본 유지됨)
+		newColors[0] = "hotpink";
+		console.log(newColors);
+		console.log(Colors);
 
-  return (
-    <>
-      <h1>useState</h1>
-      <h2>{Num}</h2>
-      {/* 이벤트 핸들러 onClick */}
-      <button onClick={plusNum}>plus</button>
-      
-    </>
-  );
+		setColors(newColors);
+
+		// setNum(Num + 1);
+	};
+	return (
+		<>
+			<h1>useState</h1>
+
+			{/* <h2>{Num}</h2> */}
+
+			<button onClick={changeColors}>Change</button>
+			<ul>
+				{Colors.map((data, idx) => (
+					<li key={idx} style={{ color: data }}>
+						{data}
+					</li>
+				))}
+			</ul>
+		</>
+	);
 }
